995e74ebba8cdf8f4e6c70f02a5b88ff655ddd20
Eduardo Sandalo Porto <eduardosandaloporto@gmail.com>
2018-09-30 15:43:29 -0300

    Finished
    Finished project with added examples

--- maze-solver.c
+++ maze-solver.c
@@ -22,21 +22,21 @@ Coordinate get_entrance(char** m, int lines, int columns) {
     Coordinate entrance = {-1, -1};
 
     for (i=0; i<lines; i++) {
-        if (m[i][0] == 'E') {
+        if (m[i][0] == 'I') {
             entrance = new_coordinate(i, 0);
             return entrance;
         }
-        if (m[i][columns-1] == 'E') {
+        if (m[i][columns-1] == 'I') {
             entrance = new_coordinate(i, columns-1);
             return entrance;
         }
     }
     for (i=0; i<columns; i++) {
-        if (m[0][i] == 'E') {
+        if (m[0][i] == 'I') {
             entrance = new_coordinate(0, i);
             return entrance;
         }
-        if (m[lines-1][i] == 'E') {
+        if (m[lines-1][i] == 'I') {
             entrance = new_coordinate(lines-1, i);
         }
     }
@@ -45,7 +45,7 @@ Coordinate get_entrance(char** m, int lines, int columns) {
 }
 
 char equals_coordinates(Coordinate coord1, Coordinate coord2) {
-    if ((coord1.x == coord2.y) && (coord1.x == coord2.y))
+    if ((coord1.x == coord2.x) && (coord1.y == coord2.y))
         return 1;
     return 0;
 }
@@ -60,33 +60,6 @@ void print_maze(char** maze, int lines, int columns) {
     }
 }
 
-/* The following 4 functions verify which ways the current coord can continue */
-/* These functions have been replaced with possibilities()
-char can_go_down(Coordinate current, char** maze, int lines) {
-    if (current.x >= lines-1) return 0;
-    if (maze[current.x+1][current.y] == ' ' || maze[current.x+1][current.y] == 'S')
-        return 1;
-    return 0;
-}
-char can_go_up(Coordinate current, char** maze) {
-    if (current.x <= 0) return 0;
-    if (maze[current.x-1][current.y] == ' ' || maze[current.x-1][current.y] == 'S')
-        return 1;
-    return 0;
-}
-char can_go_left(Coordinate current, char** maze) {
-    if (current.y <= 0) return 0;
-    if (maze[current.x][current.y-1] == ' ' || maze[current.x][current.y-1] == 'S')
-        return 1;
-    return 0;
-}
-char can_go_right(Coordinate current, char** maze, int columns) {
-    if (current.y >= columns-1) return 0;
-    if (maze[current.x][current.y+1] == ' ' || maze[current.x][current.y+1] == 'S')
-        return 1;
-    return 0;
-} */\
-
 /* Changes the array given as parameter to the possible ways the maze can continue */
 /* The array must have 4 spaces */
 void possibilities(Coordinate current, char** maze, Coordinate* poss_ways,
@@ -95,7 +68,7 @@ void possibilities(Coordinate current, char** maze, Coordinate* poss_ways,
     if (current.x >= lines-1)
         poss_ways[0] = new_coordinate(-1, -1);
     else {
-        if (maze[current.x+1][current.y] == ' ' || maze[current.x+1][current.y] == 'S')
+        if (maze[current.x+1][current.y] == ' ' || maze[current.x+1][current.y] == 'O')
             poss_ways[0] = new_coordinate(current.x+1, current.y);
         else { poss_ways[0] = new_coordinate(-1, -1); }
     }
@@ -104,7 +77,7 @@ void possibilities(Coordinate current, char** maze, Coordinate* poss_ways,
     if (current.x <= 0)
         poss_ways[1] = new_coordinate(-1, -1);
     else {
-        if (maze[current.x-1][current.y] == ' ' || maze[current.x-1][current.y] == 'S')
+        if (maze[current.x-1][current.y] == ' ' || maze[current.x-1][current.y] == 'O')
             poss_ways[1] = new_coordinate(current.x-1, current.y);
         else { poss_ways[1] = new_coordinate(-1, -1); }
     }
@@ -113,7 +86,7 @@ void possibilities(Coordinate current, char** maze, Coordinate* poss_ways,
     if (current.y <= 0)
         poss_ways[2] = new_coordinate(-1, -1);
     else {
-        if (maze[current.x][current.y-1] == ' ' || maze[current.x][current.y-1] == 'S')
+        if (maze[current.x][current.y-1] == ' ' || maze[current.x][current.y-1] == 'O')
             poss_ways[2] = new_coordinate(current.x, current.y-1);
         else { poss_ways[2] = new_coordinate(-1, -1); }
     }
@@ -122,7 +95,7 @@ void possibilities(Coordinate current, char** maze, Coordinate* poss_ways,
     if (current.y >= columns-1)
         poss_ways[3] = new_coordinate(-1, -1);
     else {
-        if (maze[current.x][current.y+1] == ' ' || maze[current.x][current.y+1] == 'S')
+        if (maze[current.x][current.y+1] == ' ' || maze[current.x][current.y+1] == 'O')
             poss_ways[3] = new_coordinate(current.x, current.y+1);
         else { poss_ways[3] = new_coordinate(-1, -1); }
     }
@@ -140,7 +113,9 @@ char stop_program() {
 /* ================================================================= */
 
 /* if there the maze is possible, will print the path in maze and will return
-    the coordinates of the exit */
+    a coordinate of value (-2, -2) 
+    the coordinate of value (-2, -2) is only used to check if the exit has been found.
+    the program will then use the possibilities array to determine where the exit is. */
 /* else, will return the starting coordinate */
 Coordinate solve(Coordinate current, char** maze, int lines, int columns) {
     int i;
@@ -149,30 +124,33 @@ Coordinate solve(Coordinate current, char** maze, int lines, int columns) {
 
     if (current.x == -1)
         return current;
-    if (maze[current.x][current.y] == 'S')
-        return current;
+    if (maze[current.x][current.y] == 'O')
+        return new_coordinate(-2, -2);
     maze[current.x][current.y] = '*';
 
-    print_maze(maze, lines, columns); printf("\n");
+    /*print_maze(maze, lines, columns); printf("\n");*/
 
     possibilities(current, maze, poss, lines, columns);
     for (i=0; i<4; i++) {
         answer = solve(poss[i], maze, lines, columns);
-        if (answer.x == -1)
+        if (answer.x == -1) /* can't go this way */
             continue;
-        if (!equals_coordinates(poss[i], answer))
+        if (answer.x == -2) /* found answer */
             return answer;
     }
+
+    /* if the program reaches this area, the solving algorithm is going backwards in the maze */
     maze[current.x][current.y] = ' ';
     return current;
 }
 
 
 int main(void) {
+    char   save;
     char   pathToFile[256];
     char** maze;
-    int nLines, nColumns, i, j;
-    FILE* fil;
+    int    nLines, nColumns, i, j;
+    FILE*  fil;
 
     printf("=======================================\n");
     printf(" Maze Solver - by github.com/edusporto \n");
@@ -185,7 +163,7 @@ int main(void) {
         scanf("%255s", pathToFile);
 
         if ((fil = fopen(pathToFile, "r")) == NULL) {
-            printf("Could not open file.\n\n");
+            printf("\nCould not open file.\n");
             continue;
         }
 
@@ -207,26 +185,47 @@ int main(void) {
             fgetc(fil);
         }
 
+        /* CLOSING THE FILE */
+        fclose(fil);
+
         /* GETS THE ENTRANCE TO THE MAZE */
         Coordinate entrance_to_maze = get_entrance(maze, nLines, nColumns);
         Coordinate exit_to_maze = solve(entrance_to_maze, maze, nLines, nColumns);
 
+        
         if (equals_coordinates(entrance_to_maze, exit_to_maze))
+        	/* NO ANSWER TO THE MAZE */
             printf("\nThere is no answer to this maze.\n");
-        else
-            printf("There is an answer to this maze. (%d,%d) (%d,%d)\n", entrance_to_maze.x, entrance_to_maze.y, exit_to_maze.x, exit_to_maze.y);
+        else {
+        	/* THERE IS AN ANSWER TO THE MAZE */
+            maze[entrance_to_maze.x][entrance_to_maze.y] = 'I'; /* the I got replaced by an * */
 
-        /* prints the matrix */
-        /*for (i=0; i<nLines; i++) {
-            for (j=0; j<nColumns; j++) {
-                printf("%c", maze[i][j]);
+            printf("\nThere is an answer to this maze.\n");
+            print_maze(maze, nLines, nColumns);
+
+            printf("\nWould you like to save the solved maze to a file? (Y/N)\n");
+            scanf(" %c", &save);
+            if (toupper(save) == 'Y') {
+            	/* SAVES THE SOLVED MAZE TO A FILE */
+                printf("\nPlease write the path in which the solved maze will be saved:\n");
+
+                scanf("%255s", pathToFile);
+
+                if ((fil = fopen(pathToFile, "w")) == NULL) {
+                    printf("\nCould not open file.\n");
+                } else {
+                    fprintf(fil, "%d\n", nLines);
+                    fprintf(fil, "%d\n", nColumns);
+                    for (i=0; i<nLines; i++) {
+                        for (j=0; j<nColumns; j++) 
+                            fprintf(fil, "%c", maze[i][j]);
+                        fprintf(fil, "\n");
+                    }
+
+                    fclose(fil);
+                }
             }
-            printf("\n");
-        }*/
-        print_maze(maze, nLines, nColumns);
-
-        /* CLOSING THE FILE */
-        fclose(fil);
+        }
 
         /* DEALLOCATING THE MAZE */
         for (i=0; i<nLines; i++)
